6b3df84754d9b2481d28a61cf0054d997725e3a5
yang, you-chuan <ycyang@iii.org.tw>
2020-09-21 18:40:43 +0800

    增加項目： 1. 可以抓出劍橋字典單字的基本資訊（word, part of speech, grammar） 2. 可以抓出劍橋字典單字的body網頁文本 3. 名詞、動詞、片語的資料結構 4. log的設定檔

--- dict.py
+++ dict.py
@@ -1,3 +1,6 @@
+import logging
+
+from bs4  import BeautifulSoup
 from util import download_page
 
 
@@ -8,13 +11,36 @@ class CamBridge:
 	def __init__(self, language):
 		self.prefix_url = self.base_url + 'english-{language}/'.format(language=language.strip())
 
-	def search(self, keyword):
-		words = (word.strip() for word in keyword.split())
-		doc = download_page(self.prefix_url + '-'.join(words))
-		print(doc)
+	def extract_head(self, head):
+		return {
+			'text': head.find('span', class_='hw dhw').text,
+			'pos ': head.find('span', class_='pos dpos').text,
+			'gram': ' '.join(g.text for g in head.find_all('span', class_='gc dgc'))
+		}
 
+	def extract_body(self, body):
+		return {}
+
+	def find_word(self, soup):
+		for word_soup in soup.find_all('div', class_='pr entry-body__el'):
+			word_head = word_soup.find('div', class_='pos-header dpos-h')
+			word_body = word_soup.find('div', class_='pos-body')
+
+			self.extract_head(word_head)
+			self.extract_body(word_body)
+
+			yield {}
+
+	def search(self, keyword):
+		target = '-'.join(word.strip() for word in keyword.split())
+		soup   = BeautifulSoup(download_page(self.prefix_url + target), 'html.parser')
+		for word_soup in self.find_word(soup):
+			pass
+		logging.info('Search the query "{}" in CamBridge'.format(target))
+
+		#print(soup)
 
 if __name__ == '__main__':
-
+	import log
 	dictionary = CamBridge('chinese-traditional')
-	dictionary.search('hello')
+	dictionary.search('out of step')
